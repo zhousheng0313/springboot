5e9d49cd95995fcdb21773642672b6962ebbce83
zhousheng1993 <zhousheng1993@outlook.com>
2018-11-18 13:50:25 +0800

    redis例子

---
+++ src/main/java/com/zzcx/springboot/controller/RedisController.java
@@ -0,0 +1,79 @@
+package com.zzcx.springboot.controller;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.zzcx.springboot.domain.IMoocJSONResult;
+import com.zzcx.springboot.domain.SysUser;
+import com.zzcx.springboot.domain.User;
+import com.zzcx.springboot.utils.JsonUtils;
+import com.zzcx.springboot.utils.RedisOperator;
+
+@RestController
+@RequestMapping("redis")
+public class RedisController {
+	
+	@Autowired
+	private StringRedisTemplate strRedis;
+	
+	@Autowired
+	private RedisOperator redis;
+	
+	@RequestMapping("/test")
+	public IMoocJSONResult test() {
+		
+		strRedis.opsForValue().set("imooc-cache", "hello 慕课网~~~~~~");
+		
+		SysUser user = new SysUser();
+		user.setId("100111");
+		user.setUsername("imooc");
+		user.setPassword("abc123");
+		user.setIsDelete(0);
+		user.setRegistTime(new Date());
+		strRedis.opsForValue().set("json:user", JsonUtils.objectToJson(user));
+		
+		SysUser jsonUser = JsonUtils.jsonToPojo(strRedis.opsForValue().get("json:user"), SysUser.class);
+		
+		return IMoocJSONResult.ok(jsonUser);
+	}
+	
+	@RequestMapping("/getJsonList")
+	public IMoocJSONResult getJsonList() {
+		
+		User user = new User();
+		user.setAge(18);
+		user.setName("慕课网");
+		user.setPassword("123456");
+		user.setBirthday(new Date());
+		
+		User u1 = new User();
+		u1.setAge(19);
+		u1.setName("imooc");
+		u1.setPassword("123456");
+		u1.setBirthday(new Date());
+		
+		User u2 = new User();
+		u2.setAge(17);
+		u2.setName("hello imooc");
+		u2.setPassword("123456");
+		u2.setBirthday(new Date());
+		
+		List<User> userList = new ArrayList<>();
+		userList.add(user);
+		userList.add(u1);
+		userList.add(u2);
+		
+		redis.set("json:info:userlist", JsonUtils.objectToJson(userList), 2000);
+		
+		String userListJson = redis.get("json:info:userlist");
+		List<User> userListBorn = JsonUtils.jsonToList(userListJson, User.class);
+		
+		return IMoocJSONResult.ok(userListBorn);
+	}
+}
