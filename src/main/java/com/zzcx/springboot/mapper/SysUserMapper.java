a07acec1037a4ffeb3f7eca65b4b43c9dad82fe8
zhousheng1993 <zhousheng1993@outlook.com>
2018-11-18 14:13:09 +0800

    删除

--- src/main/java/com/zzcx/springboot/mapper/SysUserMapper.java
+++
@@ -1,7 +0,0 @@
-package com.zzcx.springboot.mapper;
-
-import com.zzcx.springboot.domain.SysUser;
-import com.zzcx.springboot.utils.MyMapper;
-
-public interface SysUserMapper extends MyMapper<SysUser> {
-}
